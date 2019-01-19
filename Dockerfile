2e7ba17f1258c04d1265fd0c0e80203548d8cd97
rayyyy <nraavy@gmail.com>
2019-01-19 17:13:36 +0900

    support png

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,3 @@ RUN apt-get install -y pngquant
 
 WORKDIR /root/projects
 COPY . .
-
-# ENTRYPOINT ["sh", "/root/projects/start_optimize.sh"]
-# CMD ["85"]
--- start_optimize.sh
+++ start_optimize.sh
@@ -4,20 +4,13 @@ OUTPUT_DIR="output/"
 
 # $1はクオリティ 例えば85なら85%
 for filepath in $INPUT_DIR; do
+
   ## 拡張子を取得
   extension=${filepath##*.}
 
-  # ## jpgの場合
-  # if [ $extension = "jpg" ]; then
-  #   echo jpgOK
-  # fi
-
-  ## pngの場合
-  if [ $extension = "png" ]; then
-    echo pngOK
-  elif [ $extension = "jpg" ]; then
-    jpegoptim -m $1 -d $OUTPUT_DIR $filepath
-  elif [ $extension = "jpeg" ]; then
+  if [ $extension = "png" ] || [ $extension = "PNG" ]; then
+    pngquant -o $OUTPUT_DIR${filepath##*/} ${filepath}
+  elif [ $extension = "jpg" ] || [ $extension = "jpeg" ] || [ $extension = "JPEG" ] || [ $extension = "JPG" ]; then
     jpegoptim -m $1 -d $OUTPUT_DIR $filepath
   else
     echo 処理できませんでした。 ファイル名: $filepath 拡張子: $extension
